a5f1974c2e444c936b12ce06bcf1d10f541fb8b4
BLACK BOOK <91702740+SaturdayWarrior@users.noreply.github.com>
2021-10-25 19:06:22 +0100

    Update ArmstrongNumber.java

--- src/ArmstrongNumber.java
+++ src/ArmstrongNumber.java
@@ -1,10 +1,15 @@
 public class ArmstrongNumber {
     public static void main(String[] args) {
-        int number =153, originalNum, reminder, result=0;
+        //input any number and check if it Armstrong number or not
+        int originalNum, reminder, result=0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number to check ArmStrong number: ");
+        int number = scanner.nextInt();
+        int length = String.valueOf(number).length();
         originalNum=number;
         while (originalNum !=0){
             reminder = originalNum %10;
-            result+= Math.pow(reminder,3);
+            result+= Math.pow(reminder,length);
             originalNum /=10;
         }if (result == number){
             System.out.println(number + " Armstrong number.");
